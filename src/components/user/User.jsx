3171572850a0e1297f7fec586fa516b5904c03a4
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-01 23:21:05 +0200

    added avatar and user components

---
+++ src/components/user/User.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useCallback } from "react";
+import styles from "./User.module.scss";
+import { Container, Stack, Avatar, Badge, Box } from "@mui/material";
+
+import { styled } from "@mui/material/styles"
+import VerifiedIcon from '@mui/icons-material/Verified';
+
+export default function User({ 
+    name = "my name",
+    info = "my info",
+    avatar = "yes",
+    size = 55,
+    verified = false,
+    
+}) {
+    return (
+        <div className={styles.user}>
+            <Avatar {...{verified, avatar, size }}/>
+            <Box className={styles.container}>
+                <p className={styles.name}>{name}</p>
+                <p className={styles.info}>{info}</p>
+            </Box>
+        </div>
+    );
+}
