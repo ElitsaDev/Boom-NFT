c23686fe906ccad8fb120c0917d6d4a7492d19b0
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-03 00:07:02 +0200

    change badge and size of img

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -1,23 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Avatar.module.scss";
 import { Box, Badge } from "@mui/material";
 
 
 export default function Avatar({
-    size = 90,
+    
     url = "/images/avatar.png",
-    verified = false,
+    
 }) {
+    const [verified, setVerified] = useState();
+    const [size, setSize] = useState(90);
 
-    const isVerified = verified;
-    if (isVerified) {
-        return <Badge className={styles.badge}
-            overlap="circular"
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-            badgeContent={
-                <img src="/images/verified.svg" alt="" />}>
-        </Badge>
-    }
+    useEffect(() => {
+        if(verified){
+            setVerified(<Badge className={styles.badge}
+                overlap="circular"
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                badgeContent={
+                    <img src="/images/verified.svg" alt="" />}>
+            </Badge>)
+        }
+    }, [verified]);
+
+    useEffect(() => {
+        setSize(oldSize => newSize);
+    }, [size])
 
     return (
         <Box className={styles.avatar} style={{ width: size, height: size }}>
@@ -25,7 +32,8 @@ export default function Avatar({
                 alt=""
                 className={styles.image}
                 style={{ width: "100%", height: "100%" }}
-                isVerified={false}
+                isVerified={verified}
+                
             />
         </Box>
     );
