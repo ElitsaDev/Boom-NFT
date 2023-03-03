8b9e438a0e489186e804b8b311f451e999ada214
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-03 13:25:25 +0200

    correct img tag and verified

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -7,22 +7,23 @@ export default function Avatar({
     size = 90,
     url = "/images/avatar.png",
     verified = false,
-    
+
 }) {
-  
+
     return (
         <Box className={styles.avatar} style={{ width: size, height: size }}>
             <img src={url}
                 alt="avatar"
                 className={styles.image}
                 style={{ width: "100%", height: "100%" }}
-                {...verified && (<Badge className={styles.badge}
-                                    overlap="circular"
-                                    anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-                                    badgeContent={
-                                        <img src="/images/verified.svg" alt="" />}>
-                                </Badge>)}
             />
+            {verified && (<Badge className={styles.badge}
+                overlap="circular"
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                badgeContent={
+                    <img src="/images/verified.svg" alt="" />}>
+            </Badge>)}
         </Box>
     );
 }
+
