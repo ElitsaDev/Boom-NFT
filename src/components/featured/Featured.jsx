1be99e9703e708814f94fb44449c128227d798cc
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-09 15:16:10 +0200

    added futured component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,85 +1,79 @@
 import React from "react";
-
+import { useRouter } from "next/router";
 import styles from "./Featured.module.scss";
 import { Container, ImageList, ImageListItem } from "@mui/material";
 
-
-let list = [
-    {
-      image:
-        "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
-      title: "Breakfast",
-      rows: 2,
-      cols: 3,
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
-      title: "Burger",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
-      title: "Camera",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
-      title: "Coffee",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
-      title: "Hats",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
-      title: "Honey",
-      href: "/about",
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
-      title: "Basketball",
-      href: "/about",
-    },
-  ];
-
-
-  function srcset(image, size, rows = 1, cols = 1) {
-    return {
-      src: `${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
-      srcSet: `${image}?w=${size * cols}&h=${
-        size * rows
-      }&fit=crop&auto=format&dpr=2 2x`,
-    };
+export default function Featured(items = []) {
+  const router = useRouter();
+  const clickHandler = () => {
+    router.push(item.image);
   }
 
-export default function Featured({ items = list}) {
-    return (<div className={styles.wrapper}>
-        <Container className={styles.container}>
-            <ImageList
-                sx={{ width: 500, height: 450 }}
-                variant="quilted"
-                cols={4}
-                rowHeight={121}
+  return (
+    <div className={styles.wrapper}>
+      <Container className={styles.container}>
+        <ImageList cols={6} variant="quilted" gap={20}>
+          {items.map((item) => (
+            <ImageListItem key={item.image}
+              cols={item.cols || 1}
+              rows={item.rows || 1}
             >
-                {items.map((item) => (
-                    <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
-                        <img
-                            {...srcset(item.img, 121, item.rows, item.cols)}
-                            alt={item.title}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-                ))}
-            </ImageList>
-        </Container>
+              <img className={styles.image}
+                src={item.image}
+                onClick={clickHandler}
+              />
+            </ImageListItem>))
+          }
+        </ImageList>
+      </Container>
     </div>);
 }
+
+/*
+let items = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=500&h=500",
+    title: "Breakfast",
+    rows: 2,
+    cols: 3,
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=500&h=500",
+    title: "Burger",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1522770179533-24471fcdba45?w=500&h=500",
+    title: "Camera",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c?w=500&h=500",
+    title: "Coffee",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1533827432537-70133748f5c8?w=500&h=500",
+    title: "Hats",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1558642452-9d2a7deb7f62?w=500&h=500",
+    title: "Honey",
+    href: "/about",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1516802273409-68526ee1bdd6?w=500&h=500",
+    title: "Basketball",
+    href: "/about",
+  },
+];
+*/
