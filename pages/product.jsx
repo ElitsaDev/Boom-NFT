c1511f6299119e93eb32c94a49eef1b6fde75ad1
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-11 14:47:23 +0200

    nft image

--- pages/product.jsx
+++ pages/product.jsx
@@ -1,7 +1,7 @@
 import ProductImage from "../src/components/productImage/ProductImage";
 import { useState, useEffect } from "react";
 
-const url = "/images/d6b005492c2a88cd9e81e3ae72cd9859.png";
+const url = "./images/nft.jpg";
 
 export default function Product() {
     const [productImage, setProductImage] = useState("");
