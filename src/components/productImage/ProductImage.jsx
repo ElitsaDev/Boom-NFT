3d2df5b5775fc4ecf81e94e0023a5f2439b6f115
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-11 14:57:39 +0200

    url changed

--- src/components/productImage/ProductImage.jsx
+++ src/components/productImage/ProductImage.jsx
@@ -3,7 +3,7 @@ import styles from "./ProductImage.module.scss";
 
 export default function ProductImage({url='./images/nft.jpg'}){
     <div className={classNames(styles["product-image"])}>
-        <img src={`./images/${url}`} 
+        <img src={url} 
             alt="Product image"
             className={styles.image} />
     </div>
