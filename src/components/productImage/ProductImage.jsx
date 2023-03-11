d6dd7ee77ad99a66d067caaa0d8c4a402c028e0e
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-11 10:44:13 +0200

    prop of product image changed

--- src/components/productImage/ProductImage.jsx
+++ src/components/productImage/ProductImage.jsx
@@ -1,7 +1,10 @@
 import styles from "./ProductImage.module.scss";
 
-export default function ProductImage(url){
+export default function ProductImage({url}){
     <div className={styles['product-image']}>
-        <img src={url} className={styles.image} />
+        <img src={url} 
+            alt="Product image"
+        
+        className={styles.image} />
     </div>
 }
