42da7ee18641912b8740714819484b1352a2b589
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-11 16:51:20 +0200

    add

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -1,12 +1,14 @@
+/*
 import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
-/*
+
 export default function Copyright() {
   return (
     
+   
     <div className={classNames(styles.wrapper)}>
       <Container className={classNames(styles.container)} maxWidth="xl">
         <Paper className={classNames(styles.paper)}>
@@ -16,6 +18,7 @@ export default function Copyright() {
         </Paper>
       </Container>
     </div>
+    
   );
 }
 */
