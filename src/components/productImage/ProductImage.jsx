c9971a54848454525af891291338e43961a07a26
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-11 14:56:07 +0200

    default value

--- src/components/productImage/ProductImage.jsx
+++ src/components/productImage/ProductImage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./ProductImage.module.scss";
 
-export default function ProductImage({url}){
+export default function ProductImage({url='./images/nft.jpg'}){
     <div className={classNames(styles["product-image"])}>
         <img src={`./images/${url}`} 
             alt="Product image"
