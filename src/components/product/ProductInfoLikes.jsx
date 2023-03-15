78ed5cdae5bad5287a9b00972fed43d6e14bbb7d
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-03-15 16:43:56 +0200

    outlined variant

--- src/components/product/ProductInfoLikes.jsx
+++ src/components/product/ProductInfoLikes.jsx
@@ -14,6 +14,7 @@ export default function ProductInfoLikes({
     return (
         <div className={classNames(styles["product-info-likes"])} >
             <Chip className={classNames(styles.likes)}
+                variant="outlined"
                 icon={<FavoriteIcon className={classNames(styles.icon)} />}
                 label={formatedLikes}
             />
